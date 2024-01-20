dce61cf900bd410cad89116128b53063436bd6d3
justin-prabhakaran <c.m.justinprabhakaran@gmail.com>
2024-01-20 18:27:51 +0530

    lt

--- bin/randomchatele.dart
+++ bin/randomchatele.dart
@@ -4,25 +4,29 @@ import 'package:televerse/telegram.dart';
 import 'package:televerse/televerse.dart';
 
 void main() {
-  ChatID myID;
+  try {
+    ChatID myID;
 
-  bot.command("start", (ctx) {
-    ctx.reply("""/connectme => new Connection
+    bot.command("start", (ctx) {
+      ctx.reply("""/connectme => new Connection
 /bye => terminating connection
 
 Enjoy with Strangers !""");
-  });
+    });
 
-  bot.command("connectme", (ctx) async {
-    ///users.add(ChatID(DateTime.now().millisecondsSinceEpoch));
-    myID = ChatID(ctx.chat.id);
-    if (!users.contains(myID)) {
-      users.addLast(myID);
-    }
-    print(users);
-    Message m = await ctx.reply("@Bot : Connecting.......");
-    makeConnection(myID,m);
-  });
+    bot.command("connectme", (ctx) async {
+      ///users.add(ChatID(DateTime.now().millisecondsSinceEpoch));
+      myID = ChatID(ctx.chat.id);
+      if (!users.contains(myID)) {
+        users.addLast(myID);
+      }
+      print(users);
+      Message m = await ctx.reply("@Bot : Connecting.......");
+      makeConnection(myID, m);
+    });
 
-  bot.start();
+    bot.start();
+  } catch (e) {
+    print(e);
+  }
 }
