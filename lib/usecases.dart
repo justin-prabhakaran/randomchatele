68d392fe4e064312888b15be4843fc8fae59d8f6
justin-prabhakaran <c.m.justinprabhakaran@gmail.com>
2024-01-20 16:02:15 +0530

    C

--- lib/usecases.dart
+++ lib/usecases.dart
@@ -7,8 +7,8 @@ import 'package:televerse/televerse.dart';
 
 void makeConnection(ChatID myID) async {
   try {
-    StreamController<Message> myController = StreamController<Message>();
-    StreamController<Message> endUserController = StreamController<Message>();
+    StreamController<Message?> myController = StreamController<Message?>();
+    StreamController<Message?> endUserController = StreamController<Message?>();
     ChatID endUserID = users.first;
 
     print("End : ${endUserID.id}");
@@ -22,101 +22,101 @@ void makeConnection(ChatID myID) async {
       users.remove(endUserID);
       print(users);
 
+      fetchFromMyID(myController, myID, endUserController);
+      fetchFromEndUserID(endUserController, endUserID, myController);
+
       myController.stream.listen((data) async {
-        if (data.audio != null) {
-          await bot.api
-              .sendAudio(endUserID, InputFile.fromFileId(data.audio!.fileId));
-        } else if (data.photo != null) {
-          await bot.api.sendPhoto(
-              endUserID, InputFile.fromFileId(data.photo!.first.fileId));
-          print(data.photo!.first.fileId);
-        } else {
-          await bot.api.sendMessage(endUserID, data.text!);
+        try {
+          if (data?.audio != null) {
+            await bot.api.sendAudio(
+                endUserID, InputFile.fromFileId(data?.audio?.fileId ?? ""));
+          } else if (data?.photo != null) {
+            await bot.api.sendPhoto(endUserID,
+                InputFile.fromFileId(data?.photo?.first.fileId ?? ""));
+          } else if (data?.text != null) {
+            await bot.api.sendMessage(endUserID, data?.text ?? "Error");
+          } else if (data == null) {
+            await bot.api.sendMessage(endUserID, "Bot : Connection Terminated");
+          }
+        } catch (e) {
+          print("Error in myController stream: $e");
         }
       });
 
       endUserController.stream.listen((data) async {
-        if (data.audio != null) {
-          await bot.api
-              .sendAudio(myID, InputFile.fromFileId(data.audio!.fileId));
-        } else if (data.photo != null) {
-          await bot.api
-              .sendPhoto(myID, InputFile.fromFileId(data.photo!.first.fileId));
-        } else {
-          await bot.api.sendMessage(myID, data.text!);
+        try {
+          if (data?.audio?.fileId != null) {
+            await bot.api.sendAudio(
+                myID, InputFile.fromFileId(data?.audio?.fileId ?? ""));
+          } else if (data?.photo?.first != null) {
+            await bot.api.sendPhoto(
+                myID, InputFile.fromFileId(data?.photo?.first.fileId ?? ""));
+          } else if (data?.text != null) {
+            await bot.api.sendMessage(myID, data!.text!);
+          } else if (data == null) {
+            await bot.api.sendMessage(myID, "Bot : Connection Terminated");
+          }
+        } catch (e) {
+          print("Error in endUserController stream: $e");
         }
       });
-
-      fetchFromMyID(myController, myID, endUserController);
-      fetchFromEndUserID(endUserController, endUserID, myController);
     }
   } catch (e) {
-    print(e);
+    print("Error in makeConnection: $e");
   }
 }
 
-Future<void> fetchFromMyID(StreamController<Message> myController, ChatID myID,
-    StreamController<Message> endUserController) async {
-  while (!myController.isClosed) {
-    try {
-      Message s = await fetchMessages(myID);
-      if (s.text!.toLowerCase() == "/exit") {
-        myController.add(Message.fromJson({
-          "message_id": DateTime.now().millisecondsSinceEpoch,
-          "date": "Bot : Connection Terminated",
-          "sender_chat": myID.id
-        }));
+Future<void> fetchFromMyID(StreamController<Message?> myController, ChatID myID,
+    StreamController<Message?> endUserController) async {
+  try {
+    while (!myController.isClosed) {
+      Message? s = await fetchMessages(myID);
+      if (s?.text?.toLowerCase() == "/exit") {
+        myController.add(null);
         myController.close();
-        endUserController.add(Message.fromJson({
-          "message_id": DateTime.now().millisecondsSinceEpoch,
-          "date": "Bot : Connection Terminated",
-          "sender_chat": myID.id
-        }));
+        endUserController.add(null);
         endUserController.close();
         break;
       }
       myController.add(s);
-    } catch (e) {
-      print(e);
     }
+  } catch (e) {
+    print("Error in fetchFromMyID: $e");
   }
 }
 
-Future<void> fetchFromEndUserID(StreamController<Message> endUserController,
-    ChatID endUserID, StreamController<Message> myController) async {
-  while (!endUserController.isClosed) {
-    try {
-      Message s = await fetchMessages(endUserID);
-      if (s == "/exit") {
-        endUserController.add(Message.fromJson({
-          "message_id": DateTime.now().millisecondsSinceEpoch,
-          "date": "Bot : Connection Terminated",
-          "sender_chat": endUserID.id
-        }));
+Future<void> fetchFromEndUserID(StreamController<Message?> endUserController,
+    ChatID endUserID, StreamController<Message?> myController) async {
+  try {
+    while (!endUserController.isClosed) {
+      Message? s = await fetchMessages(endUserID);
+      if (s?.text?.toLowerCase() == "/exit") {
+        endUserController.add(null);
         endUserController.close();
-        myController.add(Message.fromJson({
-          "message_id": DateTime.now().millisecondsSinceEpoch,
-          "date": "Bot : Connection Terminated",
-          "sender_chat": endUserID.id
-        }));
+        myController.add(null);
         myController.close();
         break;
       }
       endUserController.add(s);
-    } catch (e) {
-      print(e);
     }
+  } catch (e) {
+    print("Error in fetchFromEndUserID: $e");
   }
 }
 
-Future<Message> fetchMessages(ChatID endUserID) async {
-  return await conv
-      .waitFor(
-          chatId: endUserID,
-          filter: (up) {
-            return up.message!.audio != null ||
-                up.message!.photo != null ||
-                up.message!.text != null;
-          })
-      .then((value) => value.update.message!);
+Future<Message?> fetchMessages(ChatID id) async {
+  try {
+    return await conv
+        .waitFor(
+            chatId: id,
+            filter: (up) {
+              return up.message?.audio != null ||
+                  up.message?.photo != null ||
+                  up.message?.text != null;
+            })
+        .then((value) => value.update.message);
+  } catch (e) {
+    print("Error in fetchMessages: $e");
+    return null;
+  }
 }
